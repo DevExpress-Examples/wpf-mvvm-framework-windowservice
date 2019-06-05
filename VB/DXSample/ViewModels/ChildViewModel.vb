5e26174cd3a2b3ddec4d897b3856f0e0c4dc4600
DevExpressExampleBot <fakeEmail>
2019-06-05 18:36:18 +0300

    Source auto update [skip ci]

---
+++ VB/DXSample/ViewModels/ChildViewModel.vb
@@ -0,0 +1,69 @@
+﻿Imports System
+Imports System.Windows
+Imports System.Windows.Input
+Imports System.Windows.Threading
+Imports DevExpress.Mvvm
+
+Namespace DXSample.ViewModels
+	Public Class ChildViewModel
+		Inherits ViewModelBase
+
+		Protected ReadOnly Property CurrentWindowService() As ICurrentWindowService
+			Get
+				Return Me.GetService(Of ICurrentWindowService)()
+			End Get
+		End Property
+		Private privateCloseWindowCommand As ICommand
+		Public Property CloseWindowCommand() As ICommand
+			Get
+				Return privateCloseWindowCommand
+			End Get
+			Private Set(ByVal value As ICommand)
+				privateCloseWindowCommand = value
+			End Set
+		End Property
+		Private privateTemporarilyHideWindowCommand As ICommand
+		Public Property TemporarilyHideWindowCommand() As ICommand
+			Get
+				Return privateTemporarilyHideWindowCommand
+			End Get
+			Private Set(ByVal value As ICommand)
+				privateTemporarilyHideWindowCommand = value
+			End Set
+		End Property
+		Public Property Caption() As String
+			Get
+				Return GetValue(Of String)()
+			End Get
+			Set(ByVal value As String)
+				SetValue(value)
+			End Set
+		End Property
+		Public Property WindowState() As WindowState
+			Get
+				Return GetValue(Of WindowState)()
+			End Get
+			Set(ByVal value As WindowState)
+				SetValue(value)
+			End Set
+		End Property
+		Public Sub New()
+			CloseWindowCommand = New DelegateCommand(AddressOf CloseWindow)
+			TemporarilyHideWindowCommand = New DelegateCommand(AddressOf TemporarilyHideWindow)
+		End Sub
+		Private Sub CloseWindow()
+			Me.CurrentWindowService.Close()
+		End Sub
+		Private Sub TemporarilyHideWindow()
+			Me.CurrentWindowService.Hide()
+			Dim timer = New DispatcherTimer() With {.Interval = TimeSpan.FromSeconds(3)}
+			AddHandler timer.Tick, Sub(o, e)
+				Me.CurrentWindowService.SetWindowState(WindowState.Maximized)
+				Me.CurrentWindowService.Show()
+				Me.CurrentWindowService.Activate()
+				timer.Stop()
+			End Sub
+			timer.Start()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DXSample/Views/ChildView.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Windows.Controls
+
+Namespace DXSample.Views
+	''' <summary>
+	''' Interaction logic for ChildView.xaml
+	''' </summary>
+	Partial Public Class ChildView
+		Inherits UserControl
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
